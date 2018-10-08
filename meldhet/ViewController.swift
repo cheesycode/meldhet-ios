c277df3db121276df44134703bb960cc67ba2732
Michel Megens <mtmmegen@pc131361.local>
2018-10-08 15:45:11 +0200

    Initial Commit

---
+++ meldhet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  meldhet
+//
+//  Created by Michel Megens on 08/10/2018.
+//  Copyright © 2018 Michel Megens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
