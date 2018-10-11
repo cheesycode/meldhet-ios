a8e838c37f5b831dcf82146c596eef1b84f7803b
Michel Megens <dev@bietje.net>
2018-10-11 15:49:39 +0200

    ui: add map and camera views
    Add map view / camera view to the storyboard.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- meldhet/ViewController.swift
+++ meldhet/ViewController.swift
@@ -1,6 +1,5 @@
 //
 //  ViewController.swift
-//  meldhet
 //
 //  Created by Michel Megens on 08/10/2018.
 //  Copyright © 2018 Michel Megens. All rights reserved.
@@ -8,18 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    var imagePicker : UIImagePickerController!
+
+    @IBAction func onImageClicked(_ sender: UITapGestureRecognizer) {
+        print("Broken clicked")
+        
+        //self.imagePicker = UIImagePickerController()
+        //self.imagePicker.delegate = self
+        //self.imagePicker.sourceType = .camera
+        //present(self.imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func onChatClickedHandler(_ sender: Any) {
+        print("GODVERDOMME WERK DAN")
+        
+        self.performSegue(withIdentifier: "mapSegue", sender: sender)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
